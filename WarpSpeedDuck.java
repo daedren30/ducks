ccc3aa2f87ba8915ec108930d614fda07198b810
daedren30 <76187727+daedren30@users.noreply.github.com>
2021-02-11 14:42:12 -0700

    Add Warp Speed Duck
    Added a duck that can boldly go where no man has gone before.

---
+++ WarpSpeedDuck.java
@@ -0,0 +1,12 @@
+public class WarpSpeedDuck extends Duck {
+
+    public WarpSpeedDuck() {
+        quackBehavior = new EngageQuack();
+    }
+    
+    public void display() {
+        System.out.println("Lightspeed duck displayed");
+    }
+    
+    }  
+}
